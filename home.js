7403a4e2cffae136bf38ec05972940eeeb6f4b68
MD ALIF MIA <alifhossinsajjad123456@gmail.com>
2025-08-23 09:56:07 +0600

    add money section done

---
+++ home.js
@@ -0,0 +1,54 @@
+
+const validPin = 1234
+
+
+document
+  .getElementById("add-money-btn")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const bank = document.getElementById("bank").value;
+    const accountNumber = document.getElementById("account-number").value;
+
+    const amount = parseInt(document.getElementById("add-amount").value);
+
+    const pinNumber = parseInt(document.getElementById("add-pin").value);
+
+    const availeableBalance = parseInt(
+      document.getElementById("available-balance").innerText
+    );
+
+    if (accountNumber.length < 11) {
+      alert("Please provide valid account number");
+      return;
+    }
+
+    if(pinNumber !== validPin){
+        alert("please enter valid pin number")
+        return;
+    }
+
+    const totalNewAvailableBalance = amount + availeableBalance;
+
+    document.getElementById("available-balance").innerText =
+      totalNewAvailableBalance;
+  });
+
+
+
+// toggleing feature
+
+document.getElementById("add-button").addEventListener("click", function(){
+    document.getElementById("cash-out-parent").style.display = "none"
+
+    document.getElementById("add-money-parent").style.display = "block"
+    
+})
+
+
+document.getElementById("cash-out-button").addEventListener("click", function(){
+    document.getElementById("add-money-parent").style.display = "none"
+
+    document.getElementById("cash-out-parent").style.display = "block"
+    
+})
